b2232f4d0c14e396b0be4bbce995d8bb8ef202d2
Bharat Kalluri <bharatkalluri@protonmail.com>
2020-08-31 22:13:06 +0530

    Moved to stow

--- dot_scripts/executable_xorg_window_logger.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-while true; do
-    echo "$(date +%s),$(xprop -id $(xprop -root -f _NET_ACTIVE_WINDOW 0x " \$0\\n" _NET_ACTIVE_WINDOW | awk "{print \$2}") | grep "WM_CLASS(STRING) =" | head -1 | rev | cut -f 1 -d " " | cut -f 2 -d "\"" | rev | tr '[:upper:]' '[:lower:]')" >>~/window.log
-    sleep 2
-done
