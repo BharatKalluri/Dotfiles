fa127519ec76395e315773ba3275f46e613adaee
Bharat Kalluri <bharatkalluri@protonmail.com>
2021-01-19 10:16:34 +0530

    Fix string issues bash

--- scripts/.scripts/install.sh
+++ scripts/.scripts/install.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 
 setup_ubuntu() {
-    echo "Adding repos, updating system and installing basic software"
+    echo 'Adding repos, updating system and installing basic software'
     sudo apt update && sudo apt upgrade -y
     sudo apt install -y zsh git ffmpeg
 }
 
 setup_solus() {
-    echo "Solus Setup starting >>"
+    echo 'Solus Setup starting >>'
     sudo eopkg up
     sudo eopkg it git flatpak xdg-desktop-portal-gtk zsh stow golang lolcat
 }
 
 setup_macos() {
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+    /bin/bash -c '$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)'
     brew install git yarn postgresql redis fzf make
     brew cask install visual-studio-code firefox rectangle iterm2 docker vlc flock spotify postico postman jetbrains-toolbox zoom ticktick anaconda bitwarden whatsapp
     xcode-select --install
@@ -22,47 +22,47 @@ setup_macos() {
 setup_macos_python() {
     pyenv install 3.8.0
     pyenv global 3.8.0
-    CFLAGS="-I/Library/Developer/CommandLineTools/Library/Frameworks/Python3.framework/Versions/3.7/include" \
-        LDFLAGS="-L/Library/Developer/CommandLineTools/Library/Frameworks/Python3.framework/Versions/3.7/lib" \
+    CFLAGS='-I/Library/Developer/CommandLineTools/Library/Frameworks/Python3.framework/Versions/3.7/include' \
+        LDFLAGS='-L/Library/Developer/CommandLineTools/Library/Frameworks/Python3.framework/Versions/3.7/lib' \
         pip install --user pycrypto
 
     LDFLAGS=-L/usr/local/opt/openssl/lib pip install psycopg2
 }
 
 setup_pi() {
-    echo "Updating and upgrading system"
+    echo 'Updating and upgrading system'
     sudo apt update && sudo apt upgrade -y
     sudo apt install zsh stow git
 }
 
 setup_fedora() {
-    echo "Adding repos, updating system and installing basic software"
+    echo 'Adding repos, updating system and installing basic software'
     sudo dnf update -y
     # RPMFusion install
     sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
     sudo dnf install stow gstreamer1-plugins-base gstreamer-plugins-bad gstreamer-plugins-ugly gstreamer1-plugins-ugly gstreamer-plugins-good-extras gstreamer1-plugins-good-extras gstreamer1-plugins-bad-freeworld ffmpeg gstreamer-ffmpeg ffmpeg-libs make gcc-c++ zsh stow -y
 
-    sudo dnf groupinstall "Development Tools" -y
+    sudo dnf groupinstall 'Development Tools' -y
 }
 
 setup_git() {
     # Setting up git
-    echo "Setting up git"
-    git config --global user.name "Bharat Kalluri"
-    git config --global user.email "bharatkalluri@protonmail.com"
+    echo 'Setting up git'
+    git config --global user.name 'Bharat Kalluri'
+    git config --global user.email 'bharatkalluri@protonmail.com'
 }
 
 setup_oh_my_zsh() {
-    echo "Installing oh my zsh!"
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-    echo "Installing zplug"
+    echo 'Installing oh my zsh!'
+    sh -c '$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)'
+    echo 'Installing zplug'
     curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
 }
 
 # Install all desktop software using flatpak
 setup_flatpak() {
-    echo "Setting up flatpak and installing software"
+    echo 'Setting up flatpak and installing software'
     # Add flathub repo
     flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
     flatpak_list=(
@@ -73,24 +73,24 @@ setup_flatpak() {
     )
     for item in ${flatpak_list[*]}; do
         # A fn at runtime to ask and install flatpak ;)
-        eval "_${item}() { flatpak install flathub $item -y; }"
+        eval '_${item}() { flatpak install flathub $item -y; }'
         ask _$item
     done
 }
 
 setup_node() {
-    echo "Installing Node using NVM"
+    echo 'Installing Node using NVM'
     wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
-    echo "Installed nvm!"
+    echo 'Installed nvm!'
 }
 
 setup_rust() {
-    echo "Installing rust"
+    echo 'Installing rust'
     curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 }
 
 setup_docker() {
-    echo "Installing docker"
+    echo 'Installing docker'
     curl -sSL https://get.docker.com | sh
 }
 
@@ -101,10 +101,10 @@ setup_flutter() {
 }
 
 setup_miniconda() {
-    echo "Setting up miniconda"
+    echo 'Setting up miniconda'
     wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/Downloads/miniconda.sh
     bash ~/Downloads/miniconda.sh
-    echo "Cleaning up"
+    echo 'Cleaning up'
     rm ~/Downloads/miniconda.sh
 }
 
@@ -114,22 +114,11 @@ setup_albert() {
     git clone https://github.com/AlbertExtensions/Autoupdate ~/.local/share/albert/org.albert.extension.python/modules/Autoupdate
 }
 
-setup_utils() {
-    echo "Setting up FZF"
+setup_fzf() {
+    echo 'Setting up FZF'
     git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
     ~/.fzf/install
-    echo "FZF install done!"
-    echo "Installing vim plug"
-    curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
-        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-    echo "Vim plug in place!"
-    echo "Installing pipenv"
-    curl https://raw.githubusercontent.com/pypa/pipenv/master/get-pipenv.py | python
-    echo "Installing common python packages"
-    pip3 install --user ipython jupyter sshuttle jupyterlab boto3
-    echo "Installed python packages"
-    echo "Creating a folder called Apps"
-    mkdir ~/Apps
+    echo 'FZF install done!'
 }
 
 install_fonts() {
@@ -139,38 +128,38 @@ install_fonts() {
 }
 
 setup_ssh_keys() {
-    ssh-keygen -t ed25519 -C "bharatkalluri@protonmail.com"
+    ssh-keygen -t ed25519 -C 'bharatkalluri@protonmail.com'
 }
 
 # Whiptail UI
 
 STEP_LIST=(
-    "install_fonts" "Install fonts"
-    "setup_ubuntu" "Update and setup ubuntu"
-    "setup_macos" "Update and setup macOS"
-    "setup_pi" "Update and setup raspberry pi"
-    "setup_fedora" "Update and setup fedora"
-    "setup_oh_my_zsh" "Install oh my ZSH!"
-    "setup_git" "Setup git email and name"
-    "setup_node" "Install node using NVM"
-    "setup_rust" "Install Rust"
-    "setup_docker" "Install docker"
-    "setup_flutter" "Install flutter and its toolchain"
-    "setup_miniconda" "Setup python using miniconda"
-    "setup_albert" "Install albert plugins"
-    "setup_utils" "Setup other utils like FZF, utility python packages etc.."
-    "setup_ssh_keys" "Create and setup ssh keys"
-    "setup_flatpak" "Setup and install flatpak apps from flathub"
+    'install_fonts' 'Install fonts'
+    'setup_ubuntu' 'Update and setup ubuntu'
+    'setup_macos' 'Update and setup macOS'
+    'setup_pi' 'Update and setup raspberry pi'
+    'setup_fedora' 'Update and setup fedora'
+    'setup_oh_my_zsh' 'Install oh my ZSH!'
+    'setup_git' 'Setup git email and name'
+    'setup_node' 'Install node using NVM'
+    'setup_rust' 'Install Rust'
+    'setup_docker' 'Install docker'
+    'setup_flutter' 'Install flutter and its toolchain'
+    'setup_miniconda' 'Setup python using miniconda'
+    'setup_albert' 'Install albert plugins'
+    'setup_fzf' 'Setup FZF'
+    'setup_ssh_keys' 'Create and setup ssh keys'
 )
+
 entry_options=()
 entries_count=${#STEP_LIST[@]}
-message=$'Choose the steps to run!'
+message='Choose the steps to run!'
 
-for i in "${!STEP_LIST[@]}"; do
+for i in ${!STEP_LIST[@]}; do
     if [ $((i % 2)) == 0 ]; then
         entry_options+=($(($i / 2)))
         entry_options+=("${STEP_LIST[$(($i + 1))]}")
-        entry_options+=("OFF")
+        entry_options+=('OFF')
     fi
 done
 
@@ -178,18 +167,18 @@ SELECTED_STEPS_RAW=$(
     whiptail \
         --checklist \
         --separate-output \
-        --title "Setup" \
+        --title 'Setup' \
         "$message" \
-        40 78 \
-        $entries_count -- "${entry_options[@]}" \
+        40 50 \
+        "$entries_count" -- "${entry_options[@]}" \
         3>&1 1>&2 2>&3
 )
 
 if [[ ! -z SELECTED_STEPS_RAW ]]; then
-    for STEP_FN_ID in "${SELECTED_STEPS_RAW[@]}"; do
+    for STEP_FN_ID in ${SELECTED_STEPS_RAW[@]}; do
         FN_NAME_ID=$(($STEP_FN_ID * 2))
         STEP_FN_NAME="${STEP_LIST[$FN_NAME_ID]}"
-        echo "---Running ${STEP_FN_NAME}---"
+        echo '---Running ${STEP_FN_NAME}---'
         $STEP_FN_NAME
     done
 fi
